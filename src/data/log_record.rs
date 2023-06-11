67fd073206daaeed964c3aa5fc12ac3771c24945
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-06-11 13:48:03 +0800

    Add: LogRecord definition

--- src/data/log_record.rs
+++ src/data/log_record.rs
@@ -1,7 +1,32 @@
+#[non_exhaustive]
+pub enum LogRecordType {
+    Normal,
+    Deleted,
+}
+
+pub struct LogRecord {
+    pub(crate) key: Vec<u8>,
+    pub(crate) value: Vec<u8>,
+    pub(crate) record_type: LogRecordType,
+}
+
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct LogRecordPos {
     /// The ID of the log file where the record is located.
     pub(crate) file_id: u32,
     /// The byte offset within the log file where the record starts.
     pub(crate) offset: u64,
+}
+
+impl LogRecord {
+    /// Encodes the `LogRecord` into a byte vector.
+    /// TODO: bitcask layout ascii art
+    ///
+    /// # Returns
+    ///
+    /// Returns a `Vec<u8>` containing the encoded representation of the `LogRecord`.
+    ///
+    pub fn encode(&self) -> Vec<u8> {
+        unimplemented!()
+    }
 }
