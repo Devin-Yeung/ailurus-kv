1acc92c2aa2860ec502745d23951a9dc42b0f2cc
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-06-20 17:16:12 +0800

    Add: some simple engine test

--- src/engine.rs
+++ src/engine.rs
@@ -182,3 +182,36 @@ fn load_datafiles<P: AsRef<Path>>(path: P) -> Result<HashMap<u32, DataFile>> {
 
     Ok(datafiles)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::errors::Errors;
+    use crate::mock::engine_wrapper::EngineWrapper;
+    use bytes::Bytes;
+
+    #[test]
+    fn simple_put_and_get() {
+        let mut db = EngineWrapper::default();
+        db.put("Hello".into(), "World".into()).unwrap();
+        assert_eq!(db.get("Hello".into()).unwrap(), Bytes::from("World"));
+    }
+
+    #[test]
+    fn overwrite_put() {
+        let mut db = EngineWrapper::default();
+        db.put("Hello".into(), "Hello".into()).unwrap();
+        db.put("Hello".into(), "World".into()).unwrap();
+        assert_eq!(db.get("Hello".into()).unwrap(), Bytes::from("World"));
+    }
+
+    #[test]
+    fn get_non_exist_key() {
+        let db = EngineWrapper::default();
+        let x = db.get("Non Exist".into());
+        #[cfg(feature = "debug")]
+        let x = x.err().unwrap().downcast::<Errors>().unwrap();
+        #[cfg(not(feature = "debug"))]
+        let x = x.err().unwrap();
+        assert_eq!(x, Errors::KeyNotFound);
+    }
+}
