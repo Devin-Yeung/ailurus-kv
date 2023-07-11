f6bd399cb8fa73d06982451d6c94a26e9a9994d1
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-07-11 17:04:57 +0800

    Refactor: refactor the `iterator` module

--- src/iterator.rs
+++ src/iterator.rs
@@ -3,21 +3,27 @@ use crate::index::IndexIterator;
 use crate::options::IteratorOptions;
 use bytes::Bytes;
 
-pub struct Iterator<'a> {
+#[derive(Debug, Eq, PartialEq)]
+pub struct Entry {
+    key: Bytes,
+    value: Bytes,
+}
+
+pub struct EngineIterator<'a> {
     index_iterator: Box<dyn IndexIterator>,
     engine: &'a Engine,
 }
 
 impl Engine {
-    pub fn iter(&self, options: IteratorOptions) -> Iterator {
-        Iterator {
+    pub fn iter(&self, options: IteratorOptions) -> EngineIterator {
+        EngineIterator {
             index_iterator: self.index.iterator(options),
             engine: self,
         }
     }
 }
 
-impl Iterator<'_> {
+impl EngineIterator<'_> {
     pub fn rewind(&mut self) {
         self.index_iterator.rewind();
     }
@@ -26,19 +32,40 @@ impl Iterator<'_> {
         self.index_iterator.seek(key);
     }
 
-    pub fn next(&mut self) -> Option<(Bytes, Bytes)> {
-        if let Some(item) = self.index_iterator.next() {
-            let value = self.engine.at(&item.1).unwrap();
-            return Some((Bytes::from(item.0.to_vec()), value));
+    pub fn next(&mut self) -> Option<Entry> {
+        if let Some((key, pos)) = self.index_iterator.next() {
+            let value = self.engine.at(pos).unwrap();
+            return Some(Entry {
+                key: key.to_vec().into(),
+                value,
+            });
         }
         None
+    }
+}
+
+impl<'a> std::iter::Iterator for EngineIterator<'a> {
+    type Item = Entry;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::engine;
+    use crate::iterator::Entry;
     use crate::options::IteratorOptions;
+
+    macro_rules! entry {
+        ($key:expr, $val:expr) => {{
+            $crate::iterator::Entry {
+                key: ::bytes::Bytes::from($key),
+                value: ::bytes::Bytes::from($val),
+            }
+        }};
+    }
 
     #[test]
     fn rewind() {
@@ -48,16 +75,30 @@ mod tests {
             let _ = iter.next();
         }
         iter.rewind();
-        assert_eq!(iter.next().unwrap(), ("Hello".into(), "World".into()))
+        assert_eq!(iter.next(), Some(entry!["Hello", "World"]));
+    }
+
+    #[test]
+    fn std_iter() {
+        let engine = engine!(["a", "val-a"], ["b", "val-b"], ["c", "val-c"]);
+        let iterator = engine.iter(IteratorOptions::default());
+        assert_eq!(
+            iterator.into_iter().collect::<Vec<Entry>>(),
+            vec![
+                entry!["a", "val-a"],
+                entry!["b", "val-b"],
+                entry!["c", "val-c"],
+            ]
+        )
     }
 
     #[test]
     fn iter() {
         let engine = engine!(["a", "val-a"], ["b", "val-b"], ["c", "val-c"]);
         let mut iter = engine.iter(IteratorOptions::default());
-        assert_eq!(iter.next().unwrap(), ("a".into(), "val-a".into()));
-        assert_eq!(iter.next().unwrap(), ("b".into(), "val-b".into()));
-        assert_eq!(iter.next().unwrap(), ("c".into(), "val-c".into()));
+        assert_eq!(iter.next(), Some(entry!["a", "val-a"]));
+        assert_eq!(iter.next(), Some(entry!["b", "val-b"]));
+        assert_eq!(iter.next(), Some(entry!["c", "val-c"]));
         assert_eq!(iter.next(), None);
     }
 
@@ -68,9 +109,9 @@ mod tests {
             filter: Box::new(|_| true),
             reverse: true,
         });
-        assert_eq!(iter.next().unwrap(), ("c".into(), "val-c".into()));
-        assert_eq!(iter.next().unwrap(), ("b".into(), "val-b".into()));
-        assert_eq!(iter.next().unwrap(), ("a".into(), "val-a".into()));
+        assert_eq!(iter.next(), Some(entry!["c", "val-c"]));
+        assert_eq!(iter.next(), Some(entry!["b", "val-b"]));
+        assert_eq!(iter.next(), Some(entry!["a", "val-a"]));
     }
 
     #[test]
@@ -80,12 +121,12 @@ mod tests {
             filter: Box::new(|_| true),
             reverse: true,
         });
-        assert_eq!(iter.next().unwrap(), ("c".into(), "val-c".into()));
-        assert_eq!(iter.next().unwrap(), ("b".into(), "val-b".into()));
+        assert_eq!(iter.next(), Some(entry!["c", "val-c"]));
+        assert_eq!(iter.next(), Some(entry!["b", "val-b"]));
         iter.rewind();
-        assert_eq!(iter.next().unwrap(), ("c".into(), "val-c".into()));
-        assert_eq!(iter.next().unwrap(), ("b".into(), "val-b".into()));
-        assert_eq!(iter.next().unwrap(), ("a".into(), "val-a".into()));
+        assert_eq!(iter.next(), Some(entry!["c", "val-c"]));
+        assert_eq!(iter.next(), Some(entry!["b", "val-b"]));
+        assert_eq!(iter.next(), Some(entry!["a", "val-a"]));
     }
 
     #[test]
@@ -93,7 +134,7 @@ mod tests {
         let engine = engine!(["a", "val-a"], ["b", "val-b"], ["c", "val-c"]);
         let mut iter = engine.iter(IteratorOptions::default());
         iter.seek("b".into());
-        assert_eq!(iter.next().unwrap(), ("b".into(), "val-b".into()));
+        assert_eq!(iter.next(), Some(entry!["b", "val-b"]));
     }
 
     #[test]
@@ -104,7 +145,7 @@ mod tests {
             reverse: true,
         });
         iter.seek("b".into());
-        assert_eq!(iter.next().unwrap(), ("b".into(), "val-b".into()));
-        assert_eq!(iter.next().unwrap(), ("a".into(), "val-a".into()));
+        assert_eq!(iter.next(), Some(entry!["b", "val-b"]));
+        assert_eq!(iter.next(), Some(entry!["a", "val-a"]));
     }
 }
